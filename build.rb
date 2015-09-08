5d822c5634d45f3430fe612166737b5f15f98250
Michael Berkovich <michael@translationexchange.com>
2015-09-08 13:02:30 -0700

    fixed tagging

--- build.rb
+++ build.rb
@@ -15,7 +15,7 @@ run('gem build tml-rails.gemspec')
 run("gem install tml-rails-#{TmlRails::VERSION}.gem --no-ri --no-rdoc")
 
 if ARGV.include?('release')
-  run('git tag #{TmlRails::VERSION}')
+  run("git tag #{TmlRails::VERSION}")
   run('git push')
   run("gem push tml-rails-#{TmlRails::VERSION}.gem")
 end
